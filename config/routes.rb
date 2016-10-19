3de919aedf3d6c714f2186c119a256f5f4f34faf
salman3-029 <salmanqureshi3029@gmail.com>
2016-10-19 13:24:30 +0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :users do
+  	collection do
+      post :ajax_test
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#index'
+
+end
