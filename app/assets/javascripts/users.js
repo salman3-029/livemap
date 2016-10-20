dbbaaf81ef02267c50b110cded70f9347ba04416
salman3-029 <salmanqureshi3029@gmail.com>
2016-10-20 15:45:02 +0500

    agents show & move live on the map

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,21 +2,7 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 $( document ).ready(function() {
-	var handler;
-	var markers;
-		
-		
-	function draw(hash){
-	  handler = Gmaps.build('Google');
-	  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-	    markers = handler.addMarker(hash);
-	    handler.bounds.extendWith(markers);
-	    handler.fitMapToBounds();
-	  });
-	  live();
-	}
 
-	
 	// Initialize Firebase
 	var config = {
 	  apiKey: "AIzaSyARQoO1-rO1IrXhQ7x9tF8K2xUvQRIFzLw",
@@ -26,24 +12,39 @@ $( document ).ready(function() {
 	  messagingSenderId: "662876714740"
 	};
 	firebase.initializeApp(config);
-	var bigOne = document.getElementById('bigOne');
 	var dbRef =  firebase.database().ref().child('text');
+
+	var handler;
+	var markers;
+
+	function redraw(hash){
+		handler.removeMarkers(markers);
+		//console.log(markers);
+		markers = handler.addMarkers(hash);
+	}
+
+	function live(){
+		dbRef.on('value', function(snapshot){
+		redraw(snapshot.val());
+		console.log(snapshot.val());
+		});
+	}
+		
+	function draw(hash){
+	  handler = Gmaps.build('Google');
+	  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+	    markers = handler.addMarkers(hash);
+	    handler.bounds.extendWith(markers);
+	    handler.fitMapToBounds();
+	  });
+	  live();
+	}
 
 	dbRef.once('value', function(snapshot){
 		draw(snapshot.val());
 	});
 
-	function live(){
-		dbRef.on('value', function(snapshot){
-		bigOne.innerText = snapshot.val().lat;
-		redraw(snapshot.val());
-		console.log(snapshot.val());
-		});
-	}
+	
 
-	function redraw(hash){
-		handler.removeMarker(markers);
-		markers = handler.addMarker(hash);
-	}
 	
 });
