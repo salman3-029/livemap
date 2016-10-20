7aa4e1e2044f02abb56080cb8e51d0c1634472d1
salman3-029 <salmanqureshi3029@gmail.com>
2016-10-20 14:34:00 +0500

    changes for one delivery agent happening in real time

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,18 +1,49 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-var handler;
-var markers;
-function draw(hash){
-  handler = Gmaps.build('Google');
-  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-    markers = handler.addMarkers(hash);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
-  });
-  }
+$( document ).ready(function() {
+	var handler;
+	var markers;
+		
+		
+	function draw(hash){
+	  handler = Gmaps.build('Google');
+	  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+	    markers = handler.addMarker(hash);
+	    handler.bounds.extendWith(markers);
+	    handler.fitMapToBounds();
+	  });
+	  live();
+	}
 
-function redraw(hash){
-	handler.removeMarkers(markers);
-	handler.addMarkers(hash);
-}
+	
+	// Initialize Firebase
+	var config = {
+	  apiKey: "AIzaSyARQoO1-rO1IrXhQ7x9tF8K2xUvQRIFzLw",
+	  authDomain: "livemap-8212d.firebaseapp.com",
+	  databaseURL: "https://livemap-8212d.firebaseio.com",
+	  storageBucket: "livemap-8212d.appspot.com",
+	  messagingSenderId: "662876714740"
+	};
+	firebase.initializeApp(config);
+	var bigOne = document.getElementById('bigOne');
+	var dbRef =  firebase.database().ref().child('text');
+
+	dbRef.once('value', function(snapshot){
+		draw(snapshot.val());
+	});
+
+	function live(){
+		dbRef.on('value', function(snapshot){
+		bigOne.innerText = snapshot.val().lat;
+		redraw(snapshot.val());
+		console.log(snapshot.val());
+		});
+	}
+
+	function redraw(hash){
+		handler.removeMarker(markers);
+		markers = handler.addMarker(hash);
+	}
+	
+});
